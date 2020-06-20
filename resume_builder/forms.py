31939e6edccc9df4c36fdebd1da2eb10b9a19208
nilesh1168 <n.suryawanshi1168@gmail.com>
2020-06-20 14:48:13 +0530

    added login,register,requirements

--- resume_builder/forms.py
+++ resume_builder/forms.py
@@ -1,8 +1,1 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
-
-class simpleForm(FlaskForm):
-        feild1 = StringField()
-        field2 = StringField()
-        button = SubmitField()
 
--- resume_builder/routes.py
+++ resume_builder/routes.py
@@ -1,8 +1,27 @@
 from resume_builder import app
 from flask import render_template
-from resume_builder.forms import simpleForm
 
 @app.route("/",methods=['GET','POST'])
 def home():
-        return render_template("home.html",form = simpleForm())
+        return render_template("home.html")
+
+@app.route("/dashboard",methods=['GET'])
+def dashboard():
+        return "Dashboard"
+
+@app.route("/login",methods=['GET'])
+def login():
+        return render_template('login.html')
+
+@app.route('/register',methods=['GET'])
+def register():
+        return render_template('register.html')
+
+@app.route("/contactUs",methods=['GET','POST'])
+def contact():
+        return render_template('contactus.html')
+
+@app.route("/features",methods=['GET'])
+def under_const():
+        return render_template('under_construction.html')
 
